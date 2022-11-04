8baa6d2518b7ad92d744a5f5cb793b52d7e4abd5
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-11-04 10:43:11 +0100

    archivos python

--- hello5.py
+++ hello5.py
@@ -1,4 +1,14 @@
-# Demonstrates a format string
+# Demonstrates  f-strings
 
 name = input("What's your name? ")
 print(f"hello, {name}")
+
+
+
+val = 'Geeks'
+print(f"{val}for{val} is a portal for {val}.")
+ 
+ 
+name = 'Tushar'
+age = 23
+print(f"Hello, My name is {name} and I'm {age} years old.")
